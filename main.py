37a34892d7e8b0b5429a00da5be9df5b2e1d72ee
io-171737 <ibrahim.oezel@hs-weingarten.de>
2023-03-02 17:56:53 +0100

    outsourced getting the filenames in a function

--- main.py
+++ main.py
@@ -2,32 +2,33 @@ import csv
 import os
 
 def main():
-	array = []
-	stream = os.popen('ls 20_newsgroups/Training/Ath >> buffer.txt')
-	with open('buffer.txt', newline='') as csvfile:
-		reader = csv.reader(csvfile, delimiter='\n')
-		for row in reader:
-			array.append([str(num) for num in row])
+	filename = []
+	get_filename(filename)
 
 	string = []
-	for i in range(len(array)):
+	for i in range(len(filename)):
 		string.append('cat 20_newsgroups/Training/Ath/' 
-		     			+ ''.join(array[i]) 
+		     			+ ''.join(filename[i]) 
 						+' | tr -d "[:punct:]" | tr -s "[:space:]" "\n" | sort | uniq -ci')
 	
 	dictionary = {}
-	print(string[3])
 	stream = os.popen(string[3]).read()
 	output = stream.split('\n')
 
 	for line in output:
-		print(line)
 		if line == '':
 			break
 		word1, word2 = line.split()
 		dictionary[word2] = word1
 
 	print(dictionary)
+
+def get_filename(array):
+	stream = os.popen('ls 20_newsgroups/Training/Ath >> buffer.txt')
+	with open('buffer.txt', newline='') as csvfile:
+		reader = csv.reader(csvfile, delimiter='\n')
+		for row in reader:
+			array.append([str(num) for num in row])
 
 if __name__ == '__main__':
 	main()
