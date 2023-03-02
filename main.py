85711300922a74cd7a4eb7f8291ee32c294b561b
io-171737 <ibrahim.oezel@hs-weingarten.de>
2023-03-02 15:09:30 +0100

    get all file names and get list of words with the amount

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import csv
+import os
+
 def main():
-	pass
+	array = []
+	stream = os.popen('ls 20_newsgroups/Training/Ath >> buffer.csv')
+	with open('buffer.csv', newline='') as csvfile:
+		reader = csv.reader(csvfile, delimiter='\n')
+		for row in reader:
+			array.append([str(num) for num in row])
+
+	string = []
+	for i in range(len(array)):
+		string.append('cat 20_newsgroups/Training/Ath/' 
+		     				+ ''.join(array[i]) 
+							+' | tr -d "[:punct:]" | tr -s "[:space:]" "\n" | sort | uniq -ci')
+	
+	print(string[1])
+	stream = os.popen(string[3])
+	output = stream.read()
+	print(output)
 
 if __name__ == '__main__':
 	main()
