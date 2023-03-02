462bc86f407bb2415028496edbec0a623f03d108
io-171737 <ibrahim.oezel@hs-weingarten.de>
2023-03-02 17:49:40 +0100

    read file and save it in a dictionary

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import os
 
 def main():
 	array = []
-	stream = os.popen('ls 20_newsgroups/Training/Ath >> buffer.csv')
-	with open('buffer.csv', newline='') as csvfile:
+	stream = os.popen('ls 20_newsgroups/Training/Ath >> buffer.txt')
+	with open('buffer.txt', newline='') as csvfile:
 		reader = csv.reader(csvfile, delimiter='\n')
 		for row in reader:
 			array.append([str(num) for num in row])
@@ -12,13 +12,22 @@ def main():
 	string = []
 	for i in range(len(array)):
 		string.append('cat 20_newsgroups/Training/Ath/' 
-		     				+ ''.join(array[i]) 
-							+' | tr -d "[:punct:]" | tr -s "[:space:]" "\n" | sort | uniq -ci')
+		     			+ ''.join(array[i]) 
+						+' | tr -d "[:punct:]" | tr -s "[:space:]" "\n" | sort | uniq -ci')
 	
-	print(string[1])
-	stream = os.popen(string[3])
-	output = stream.read()
-	print(output)
+	dictionary = {}
+	print(string[3])
+	stream = os.popen(string[3]).read()
+	output = stream.split('\n')
+
+	for line in output:
+		print(line)
+		if line == '':
+			break
+		word1, word2 = line.split()
+		dictionary[word2] = word1
+
+	print(dictionary)
 
 if __name__ == '__main__':
 	main()
